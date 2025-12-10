bad7317efca5a91d7b4204074bc9a152f82f166d
MMCIRX2 <3003mgf@gmail.com>
2025-12-10 20:32:03 -0300

    Add tokenize sentence function (nltk)

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
+import ntlk
+
 def hello ():
     print("Hello")
+
+
+def tokenize_sentence(sentence:str) -> list[str]:
+    return [word for word in nltk.word_tokenize(sentence) if any(c.isalpha() for c in word)]
